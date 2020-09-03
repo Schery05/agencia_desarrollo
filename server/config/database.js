c0de4f79d257a5d0cc8c8f65542763bcdc47e070
Schery05 <yamilet.schery05@gmail.com>
2020-09-03 14:25:17 -0400

    agregando coma que faltaba

--- server/config/database.js
+++ server/config/database.js
@@ -13,7 +13,7 @@ module.exports = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, proce
         min: 0,
         acquire: 30000,
         idle: 10000
-    }
+    },
     //operatorAliases: false
     })
 
